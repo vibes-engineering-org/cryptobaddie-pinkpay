bf10e283836656f8f9ed17fe2bd7c183349f58b3
hellno <hellno@users.noreply.github.com>
2025-08-24 11:05:15 +0200

    update manifest

--- src/app/.well-known/farcaster.json/route.ts
+++ src/app/.well-known/farcaster.json/route.ts
@@ -1,13 +1,17 @@
 import { PROJECT_TITLE } from "~/lib/constants";
 
 export async function GET() {
-  const appUrl = process.env.NEXT_PUBLIC_URL || `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`;
+  const appUrl =
+    process.env.NEXT_PUBLIC_URL ||
+    `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`;
 
   const config = {
     accountAssociation: {
-      header: "eyJmaWQiOjg2OTk5OSwidHlwZSI6ImN1c3RvZHkiLCJrZXkiOiIweDc2ZDUwQjBFMTQ3OWE5QmEyYkQ5MzVGMUU5YTI3QzBjNjQ5QzhDMTIifQ",
+      header:
+        "eyJmaWQiOjg2OTk5OSwidHlwZSI6ImN1c3RvZHkiLCJrZXkiOiIweDc2ZDUwQjBFMTQ3OWE5QmEyYkQ5MzVGMUU5YTI3QzBjNjQ5QzhDMTIifQ",
       payload: "eyJkb21haW4iOiJjcnlwdG9iYWRkaWUtcGlua3BheS52ZXJjZWwuYXBwIn0",
-      signature: "MHgzYWI1ZDIzNGViY2RiZGM2OTZmOGIzMDQxYzNkZTc3ZTg5ZDBjMGNhYjY3ZmRjZDZiY2EwMDI2YmE3M2ViNTViN2E0YzJjOWUyZjliYWY2MTU1ZGM2NTI0ZGU5ZTIxMmViYzA2YWVlZDAxZTc5OTFlMmY3ODgzODA4MDg1Mjk5ODFj",
+      signature:
+        "MHgzYWI1ZDIzNGViY2RiZGM2OTZmOGIzMDQxYzNkZTc3ZTg5ZDBjMGNhYjY3ZmRjZDZiY2EwMDI2YmE3M2ViNTViN2E0YzJjOWUyZjliYWY2MTU1ZGM2NTI0ZGU5ZTIxMmViYzA2YWVlZDAxZTc5OTFlMmY3ODgzODA4MDg1Mjk5ODFj",
     },
     frame: {
       version: "1",
@@ -20,7 +24,7 @@ export async function GET() {
       splashImageUrl: `${appUrl}/splash.png`,
       splashBackgroundColor: "#555555",
       primaryCategory: "finance",
-      tags: ["crypto", "offramp", "mpesa", "africa", "finance"]
+      tags: ["crypto", "offramp", "mpesa", "africa", "finance"],
     },
   };
 
